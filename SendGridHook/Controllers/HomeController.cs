ce62a6b085b15bbd839e35c45e15db6671a8e130
guillaumejay <guillaumejay@users.noreply.github.com>
2014-07-26 13:32:20 -0400

    Adding the Hook project

---
+++ SendGridHook/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SendGridEventsModel;
+using System.Net;
+
+namespace SendGridHook.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            SendGridEventsDB context = new SendGridEventsDB();
+
+            List<Event> events = context.Events.OrderByDescending(x => x.EventDate).Take(50).ToList();
+
+            return View(events);
+        }
+    }
+}
---
+++ SendGridHook/Controllers/HookController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using SendGridEventsModel;
+
+
+namespace SendGridHook.Controllers
+{
+    public class HookController : ApiController
+    {
+        [HttpPost]
+        [Route("Hook/Receive")]
+        public async Task<HttpResponseMessage> Receive()
+        {
+            string jsonData = await Request.Content.ReadAsStringAsync();
+            JArray dataList = JArray.Parse(jsonData);
+           
+            using (SendGridEventsDB context = new SendGridEventsDB())
+            {
+                foreach (JToken rec in dataList)
+                {
+             
+                
+                    Event newEvent = Event.CreateFromToken(rec);
+                    context.Events.Add(newEvent);
+                    context.SaveChanges();
+
+                }
+            }
+            return new HttpResponseMessage(HttpStatusCode.OK);
+
+        }
+
+    }
+}
